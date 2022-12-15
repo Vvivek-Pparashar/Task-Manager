121f8462695d1a7adde36bc6c003d5a02f4baaef
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-12-16 04:51:01 +0530

    updated

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const tasks = require("./routes/tours");
 const path = require("path");
-const bodyParser = require("body-parser");
 const connectDB = require("./db/connect");
 require('dotenv').config();
 
 const app = express();
 
-const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('./public'))
+app.use(express.json());
+
 
 //Routes
+app.use("/api/v1/tasks", tasks);
 
 // app.get("/", (req, res) => {
 //   res.sendFile('index.html');
 // });
 
-app.use("/api/v1/tasks", jsonParser, tasks);
+
 
 const port = 5000;
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,79 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("hlo guys vivek is gr8");
+// =====================   GET ALL THE TASKS   ================================
+
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ tasks: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+
+// =====================   GET THE TASK   ================================
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: "No task for this id" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).send("invalid name");
+  }
+};
+
+// =====================   CREATE A NEW TASK  ================================
 
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (error) {
-    console.log(error)
-    res.status(500).send("invalid name")
+    console.log(error);
+    res.status(500).send("invalid name");
   }
 };
-const deleteTask = (req, res) => {
-  // console.log(req.body)
-  res.send("Deletedask");
+
+// =====================   DELETE THE TASK   ================================
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: "No task for this id" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).send("invalid name");
+  }
 };
-const updateTask = (req, res) => {
-  res.send("update the task");
+
+// =====================   UPDATE ALL THE TASK   ================================
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: "No task for this id" });
+    }
+
+    res.status(200).send({ task });
+  } catch (error) {
+    res.status(500).send("invalid name");
+  }
 };
-const getTask = (req, res) => {
-  res.send("getTask");
-};
+
+// =====================   EXPORTS EVERYTHING   ================================
 module.exports = {
   getAllTasks,
   getTask,
